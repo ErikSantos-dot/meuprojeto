9a5d39849ae539d9329accdc59266a22c0711bdd
ErikSantos-dot <erikdossantos896@gmai.com>
2025-06-22 18:54:53 -0300

    meu primeiro envio

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página PHP Dinamica</title>
+
+    <style>
+        body{
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 0;
+            padding: 20px;
+            background-color: #f4f4f4;
+        }
+        h1{
+            color: #333
+        }
+        p{
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Bem-Vindo á minha página PHP!</h1>
+    <P>
+        <?php
+
+        
+        ?>
+    </P>
+</body>
+</html>
